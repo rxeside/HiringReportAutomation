d76890312ea8afa35bb5eb595a56b7da595d3d5d
denis.leuchin <rxeside@gmail.com>
2025-09-03 22:37:52 +0300

    обновил токен, убрал скроллбар

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
-HUNTFLOW_API_TOKEN = "91ad37c4954e073caada3d27de0dbc515f5b4d84aa97e4938003e446bacbc172"
+# TODO: подумать как можно обновлять токен автоматически
+HUNTFLOW_API_TOKEN = "44020d74f32cbde429f654f92cbbc1e6882419b56e13208651f3fb1198efcf4e"
 REPORT_CACHE_FILE = "report_cache.json"
 UPDATE_INTERVAL_SECONDS = 3600  # час
 CACHE_FILE_PATH = "cache/report_cache.json"
